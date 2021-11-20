3773431dcb7128dda1c0786b472f28ac0f381029
Chengjun Peng <joeypeng@umich.edu>
2021-11-20 05:17:02 -0500

    [Update parser] Update parser with new syntax.('To' binding only has one pair)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use riddle_compiler::syntax::{ExpCompute, ExpVal};
 use riddle_compiler::parser::{ComputationParser, ValueParser};
 use riddle_compiler::eval::{eval, eval_with_stack, substCompute, substVal};
 use riddle_compiler::syntax::{ExpCompute, ExpVal, Prim2, TCompute, TVal, Terminal};
